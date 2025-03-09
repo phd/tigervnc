d6447e9aa08c5f5eea59e5bc557458af2586d262
Piotr Henryk Dabrowski <phd@phd.re>
2025-03-09 14:15:56 +0100

    Option to disable grabbing mouse+keyboard with Right Ctrl
    New option: GrabToggleWithRightCtrl (on by default)

--- vncviewer/parameters.cxx
+++ vncviewer/parameters.cxx
@@ -207,6 +207,11 @@ core::BoolParameter
            "Activates both GrabOnlyKeyboard and GrabOnlyMouse.",
            false);
 core::BoolParameter
+  grabToggleWithRightCtrl("GrabToggleWithRightCtrl",
+                          "Toggle mouse and keyboard grab by "
+                          "pressing the Right Ctrl key.",
+                          true);
+core::BoolParameter
   grabToggleWithMiddleButton("GrabToggleWithMiddleButton",
                              "Toggle mouse and keyboard grab by "
                              "middle-clicking on the window.",
@@ -308,6 +313,7 @@ static core::VoidParameter* parameterArray[] = {
   &grabOnlyKeyboard,
   &grabOnlyMouse,
   &grabOnly,
+  &grabToggleWithRightCtrl,
   &grabToggleWithMiddleButton,
   &grabWithMouseClick,
   &emulateMiddleButton,
--- vncviewer/parameters.h
+++ vncviewer/parameters.h
@@ -66,6 +66,7 @@ extern core::BoolParameter viewOnly;
 extern core::BoolParameter grabOnlyKeyboard;
 extern core::BoolParameter grabOnlyMouse;
 extern core::BoolParameter grabOnly;
+extern core::BoolParameter grabToggleWithRightCtrl;
 extern core::BoolParameter grabToggleWithMiddleButton;
 extern core::IntParameter grabWithMouseClick;
 
