d5d58c9f7c74296b99b9886a67c15595e7f1127c
Piotr Henryk Dabrowski <phd@phd.re>
2025-06-09 12:19:33 +0200

    TEST: draw only when focused

--- vncviewer/DesktopWindow.cxx
+++ vncviewer/DesktopWindow.cxx
@@ -942,9 +942,11 @@ int DesktopWindow::handle(int event)
     break;
 
   case FL_SHOW:
+  case FL_FOCUS:
     cc->enableUpdates(true);
     break;
   case FL_HIDE:
+  case FL_UNFOCUS:
     cc->enableUpdates(false);
     break;
   }
